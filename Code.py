cc353797ed56cf6102d53499f1eb30cab010e029
Kennyisthebest <tang.kenny@csmstudents.org>
2019-05-17 16:20:20 +0000

    Here is code

--- Code.py
+++ Code.py
@@ -8,3 +8,9 @@ else:
     ask3=input('Who is your mother\'s favorite child?')
     if(ask3==ask1):
         print('I doubt that')
+    else:
+        ask4=int(input('If the robots take over the earth, would you 1. Accept 2. Accept 3. Accept 4. Rebel?'))
+        if(ask4!=4):
+            print('Exallent')
+        else:
+            print('Oh')
