a91f95f0578fdf8bf5c8ae4eef285266d8451877
Kennyisthebest <tang.kenny@csmstudents.org>
2019-05-17 16:15:14 +0000

    the code man

--- Code.py
+++ Code.py
@@ -1,1 +1,10 @@
-print('hello')
+ask1=input('Enter your name')
+print('Hello')
+ask2=int(input('Enter you age'))
+if(ask2<18):
+    print('I am not allowed to talk to you.')
+    print('You need to leave. NOW.')
+else:
+    ask3=input('Who is your mother\'s favorite child?')
+    if(ask3==ask1):
+        print('I doubt that')
